cdc97c16a1986fcf8f93ae5c82e7847e5dc81e95
Nico <qtsddxy@163.com>
2021-08-02 18:21:09 +0800

    wofgle

---
+++ precache-manifest.a23dd35126aa3f92a25714a3004152b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1f200bb087e346d75f",
+    "url": "css/app.a8200ca7.css"
+  },
+  {
+    "revision": "787e309974aabab59b1d7accba2bf1a3",
+    "url": "index.html"
+  },
+  {
+    "revision": "4b1f200bb087e346d75f",
+    "url": "js/app.9b0eaae0.js"
+  },
+  {
+    "revision": "f6e5e001d5ca7fc9aedd",
+    "url": "js/chunk-vendors.e18fdaba.js"
+  },
+  {
+    "revision": "02adcb277ba6fd882d3307d3966c1370",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
